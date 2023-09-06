1b0b93dce1583b021d0f06c428277b9e8f105ce9
Jean Bardellini <jbardellini@LABA.PSWAS.LOCAL>
2023-09-06 15:42:05 -0700

    code along react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import NameMaker from './components/NameMaker'
+import TopMenu from './components/TopMenu'
+
+function App() {
+
+  return (
+    <>
+      <h1>Code Along 2</h1>
+
+      <TopMenu />
+      <NameMaker />
+    </>
+  )
+}
+
+export default App
